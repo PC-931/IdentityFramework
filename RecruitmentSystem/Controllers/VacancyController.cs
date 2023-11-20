711f58c88e1abe5e3a450104dfe4cf6bbc92ae1b
PC-931 <70005863+PC-931@users.noreply.github.com>
2023-11-21 02:04:19 +0530

    Completed Agency and Vacancy part

--- RecruitmentSystem/Controllers/VacancyController.cs
+++ RecruitmentSystem/Controllers/VacancyController.cs
@@ -1,4 +1,9 @@
 ﻿using RecruitmentSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Diagnostics;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace RecruitmentSystem.Controllers
@@ -6,11 +11,14 @@ namespace RecruitmentSystem.Controllers
     [Authorize]
     public class VacancyController : Controller
     {
-
+        List<Vacancy> vacancyList;
+        Vacancy v;
         public VacancyDbContext db;
         public VacancyController() 
         { 
             db = new VacancyDbContext();
+            vacancyList = new List<Vacancy>();
+            v = new Vacancy();
         }
 
         [HttpGet]
@@ -28,5 +36,99 @@ namespace RecruitmentSystem.Controllers
             return View();
         }
 
+        public ActionResult ShowAllVacancy()
+        {
+            try
+            {
+                vacancyList = db.vacancies.ToList();
+                if(vacancyList.Count > 0 )
+                {
+                    return View(vacancyList);
+                }
+                else
+                {
+                    return View();
+                }
+            }catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            
+        }
+
+        [HttpGet]
+        public ActionResult EditVacancy(int id)
+        {
+            try
+            {
+                v = db.vacancies.Find(id);
+                if( v!=null)
+                {
+                    return View(v);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception($"{ex.Message}");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult EditVacancy(Vacancy updVacancy)
+        {
+            try
+            {
+                v = db.vacancies.Find(updVacancy.id);
+                if (v != null)
+                {
+                    db.vacancies.AddOrUpdate(updVacancy);
+                    db.SaveChanges();
+                    return View("ShowAllVacancy");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{ex.Message}");
+            }
+            return View();
+        }
+
+        public ActionResult DetailsOfVacancy(int id)
+        {
+            try
+            {
+                v = db.vacancies.Find(id);
+                if( v!=null )
+                {
+                    return View(v);
+                }
+            }
+            catch(Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return View();
+        }
+        
+        public ActionResult DeleteVacancy(int id)
+        {
+            try
+            {
+                v = db.vacancies.Find(id);
+                if(v!=null )
+                {
+                    db.vacancies.Remove(v);
+                    db.SaveChanges();
+                    return RedirectToAction("ShowAllVacancy");
+                }
+            }
+            catch(Exception e)
+            {
+                throw new Exception (e.Message);
+            }
+            return RedirectToAction("ShowAllVacancy");
+        }
     }
+
 }
